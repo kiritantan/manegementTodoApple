22d110b832f0bb2442eaf768a4644b8ec1075a14
kiri <s1411376@coins.tsukuba.ac.jp>
2015-07-24 18:36:20 +0900

    Initial Commit

---
+++ manegementTodoApple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  manegementTodoApple
+//
+//  Created by kiri on 2015/07/24.
+//  Copyright (c) 2015年 kiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
